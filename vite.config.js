bc2338cb45c23615d2f0f8e78fc99577b668f68e
Leal, João <joaohenriquelealdesouza001@gmail.com>
2025-12-05 17:48:01 -0300

    Atualiza o nome do repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/todo-app/',
+    base: '/tasken-todo-app/',
     plugins: [react(), tailwindcss()],
 });
